164f2a96d73ee5d295d2ad03540d3dfc8a5c804a
Shinetech <xiujian@shinetechsoftware.com>
2021-05-26 20:53:42 +0800

    test

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
+function App(props) {
+ 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {props.boss} <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -17,9 +19,38 @@ function App() {
         >
           Learn React
         </a>
+        <Testzz boss='wangzhen'></Testzz>
       </header>
     </div>
   );
+}
+
+class Testzz extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      family:['bank','qin limin','wa']
+    }
+  }
+  addPerson=()=>{
+    this.setState({
+      family:[...this.state.family,Math.random()]
+    })
+  }
+
+  render(){
+    return(
+      <div>
+        <p>boss is {this.props.boss}</p>
+        <button onClick={this.addPerson}>add new person</button>
+        <ul>
+          {this.state.family.map(f=>{
+            return <li key={f}>{f}</li>
+          })}
+        </ul>
+      </div>
+    )
+  }
 }
 
 export default App;
