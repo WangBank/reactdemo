d993b1f69afb25e8a7bdb6f387aeb090ff148a99
Bank <wangyinhang@qq.com>
2021-07-14 17:24:34 +0800

    react-redux and react-router4

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import './App.css';
 import React from 'react';
+import {connect} from 'react-redux'
+import {addPerson,leavePerson,addPersonAsync} from './index.redux'
 
+// const mapStatetoProps= (state)=>{
+//   return {num:state}
+// }
+// const actionCreator = {addPerson,leavePerson,addPersonAsync}
+
+//App = connect(mapStatetoProps,actionCreator)(App)
+@connect(
+  state=>({num:state}),
+  {addPerson,leavePerson,addPersonAsync}
+  )
 class App extends React.Component{
-  constructor(props){
-    super(props)
-  }
   render(){
-    const store = this.props.store
-    const num = store.getState()
-    const addPerson = this.props.addPerson
-    const leavePerson = this.props.leavePerson
-    const addPersonAsync = this.props.addPersonAsync
     return(
       <div>
-      <p>现在有人{num}个</p>
-      <button onClick={()=>store.dispatch(addPerson())}>加人</button>
-      <button onClick={()=>store.dispatch(leavePerson())}>减人</button>
-      <button onClick={()=>store.dispatch(addPersonAsync())}>隔两秒摇人</button>
+      <p>现在有人{this.props.num}个</p>
+      <button onClick={this.props.addPerson}>加人</button>
+      <button onClick={this.props.leavePerson}>减人</button>
+      <button onClick={this.props.addPersonAsync}>隔两秒摇人</button>
       </div>
     )
   }
 }
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -5,25 +5,70 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {createStore,applyMiddleware,compose} from 'redux'
 import thunk from 'redux-thunk'
-import {counter,addPerson,leavePerson,addPersonAsync} from './index.redux'
+import {counter} from './index.redux'
+import {Provider} from "react-redux"
+import {
+  BrowserRouter,
+  Route,
+  Link,
+  Redirect,
+  Switch
+} from "react-router-dom"
 
 const store = createStore(counter, compose(
   applyMiddleware(thunk),
   window.devToolsExtension?window.devToolsExtension():f=>f
 ))
+function qlm(){
+  return <h2>qlm</h2>
+}
 
+function xpt(){
+  return <h2>xpt</h2>
+}
 
+class Test extends React.Component{
+  constructor(props){
+    super(props)
+  }
+  render(){
+    return(
+      <h2>404</h2>
+    )
+  }
+}
 
-function render(){
-  ReactDOM.render(
-    <React.StrictMode>
-      <App store={store} addPerson={addPerson} leavePerson={leavePerson} addPersonAsync={addPersonAsync}/>
-    </React.StrictMode>,
+  ReactDOM.render((
+      <React.StrictMode>
+      <Provider store={store}>
+      <BrowserRouter>
+      <div>
+        <ul>
+            <li>
+              <Link to='/'>wz</Link>
+            </li>
+            <li>
+              <Link to='/qlm'>qlm</Link>
+            </li>
+            <li>
+              <Link to='/xpt'>xpt</Link>
+            </li>
+          </ul>
+          <Switch>
+          <Route path='/' exact component={App}></Route>
+          <Route path='/qlm' component={qlm}></Route>
+          <Route path='/xpt' component={xpt}></Route>
+          <Route path='/:location' component={Test}></Route>
+          </Switch>
+         
+      </div>
+      </BrowserRouter>
+     
+    </Provider> 
+    </React.StrictMode>
+  ) ,
     document.getElementById('root')
   );
-}
-render()
-store.subscribe(render)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
